f95e8f7dabe54761b8c189c7dd113f1b77fcd611
YumaYX <86939129+YumaYX@users.noreply.github.com>
2024-05-21 22:38:45 +0900

    update

--- app.rb
+++ app.rb
@@ -1,20 +1,36 @@
 # frozen_string_literal: true
 
-require "mechanize"
-
-def title_of(url)
-  page = Mechanize.new.get(url)
-  page.title.gsub(/\|/,'\|')
-rescue StandardError => e
-  ""
-end
+require "net/http"
+require "json"
 
 doc = String.new
-urls = File.readlines('urls.txt').map(&:chomp).reject(&:empty?)
-urls.each do |url|
-  title = title_of(url)
-  next if title.empty?
-  doc = doc + "- [#{title}](#{url})\n"
+
+def to_markdown(isbn, p_url)
+  isbn = isbn.gsub(/-/, "")
+  url = "https://api.openbd.jp/v1/get?isbn=#{isbn}"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  books = JSON.parse(response)
+
+  book_info = books.first
+  book_sum = book_info['summary']
+
+  <<~MARKDOWN
+    ### [#{book_sum['title']}](#{p_url})
+
+    | 著者 | #{book_sum['author']} |
+    | 出版社 | #{book_sum['publisher']} |
+    | 出版日 | #{book_sum['pubdate']} |
+    | ISBN| #{book_sum['isbn']} |
+
+  MARKDOWN
 end
 
-File.write("index.md", doc)
+data = {
+  '978-4-7917-6555-3' => 'http://www.seidosha.co.jp/book/index.php?id=1779',
+  '978-4-334-95293-8' => 'https://www.kobunsha.com/shelf/book/isbn/9784334952938',
+  '978-4-478-06659-1' => 'https://www.diamond.co.jp/book/9784478066591.html'
+}
+
+data.sort.each { |isbn, url| doc += to_markdown(isbn, url) }
+File.write('index.md', doc)
